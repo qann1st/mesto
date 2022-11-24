b1130d119aff0bde7abf4ac61e3f625a8aefb03a
Vladislav Klyuchnikov <bllxy67@gmail.com>
2022-11-24 19:58:25 +0300

    feat: add function edit button

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,11 @@
 const btnEdit = document.querySelector(".profile__edit-button");
 const popupEditProfile = document.querySelector(".popup-edit-profile");
 const popupEditProfileClose = popupEditProfile.querySelector(".popup__close");
+const formElement = document.querySelector(".popup__form");
+const nameInput = document.querySelector(".popup__name");
+const jobInput = document.querySelector(".popup__description");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
 
 function openPopup(element) {
   element.classList.add("popup__opened");
@@ -10,5 +15,21 @@ function closePopup(element) {
   element.classList.remove("popup__opened");
 }
 
-btnEdit.addEventListener("click", () => openPopup(popupEditProfile));
-popupEditProfileClose.addEventListener("click", () => closePopup(popupEditProfile));
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+}
+
+btnEdit.addEventListener("click", () => {
+  openPopup(popupEditProfile);
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileDescription.textContent;
+});
+
+popupEditProfileClose.addEventListener("click", () =>
+  closePopup(popupEditProfile)
+);
+
+formElement.addEventListener("submit", formSubmitHandler);
+formElement.addEventListener("submit", () => closePopup(popupEditProfile));
