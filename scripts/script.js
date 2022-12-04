f85bed15db8d4f88c36796b9cdcf97c4e6fb79dc
Vladislav Klyuchnikov <bllxy67@gmail.com>
2022-12-04 17:34:08 +0300

    add popup new-place

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
 const btnEdit = document.querySelector(".profile__edit-button");
+const btnAdd = document.querySelector(".profile__add-button")
 const popupEditProfile = document.querySelector(".popup-edit-profile");
-const popupEditProfileClose = popupEditProfile.querySelector(".popup__close");
+const popupEditProfileClose = popupEditProfile.querySelector(".popup-edit-profile_close");
+const popupNewPlace = document.querySelector('.popup-new-place');
+const popupNewPlaceClose = popupNewPlace.querySelector('.popup-new-place_close')
 const formElement = document.querySelector(".popup__form");
 const nameInput = document.querySelector(".popup__name");
 const jobInput = document.querySelector(".popup__description");
@@ -9,17 +12,17 @@ const profileDescription = document.querySelector(".profile__description");
 
 function openPopup(element) {
   element.classList.add("popup__opened");
-}
+};
 
 function closePopup(element) {
   element.classList.remove("popup__opened");
-}
+};
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileDescription.textContent = jobInput.value;
-}
+};
 
 btnEdit.addEventListener("click", () => {
   openPopup(popupEditProfile);
@@ -27,9 +30,11 @@ btnEdit.addEventListener("click", () => {
   jobInput.value = profileDescription.textContent;
 });
 
-popupEditProfileClose.addEventListener("click", () =>
-  closePopup(popupEditProfile)
-);
+popupEditProfileClose.addEventListener("click", () => closePopup(popupEditProfile));
+
+btnAdd.addEventListener("click", () => openPopup(popupNewPlace));
+
+popupNewPlaceClose.addEventListener("click", () => closePopup(popupNewPlace));
 
 formElement.addEventListener("submit", formSubmitHandler);
 formElement.addEventListener("submit", () => closePopup(popupEditProfile));
