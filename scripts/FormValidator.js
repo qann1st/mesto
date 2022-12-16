3fc1755bf14cb53c1578f004c96acd74aec4e5ad
Vladislav Klyuchnikov <bllxy67@gmail.com>
2022-12-16 17:07:59 +0300

    ready

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -57,8 +57,7 @@ export default class FormValidator {
   resetValidation() {
     this._setEventListeners();
     this._inputList.forEach((input) => {
-      this._hideInputError(input)
+      this._hideInputError(input);
     });
-    this._form.reset()
   }
 }
